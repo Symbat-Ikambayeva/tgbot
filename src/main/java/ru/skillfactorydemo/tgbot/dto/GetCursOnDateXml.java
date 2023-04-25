9ee05e9d9881da16627c5b38a228770d4dbf6781
Symbat-Ikambayeva <sumbat2307@mail.ru>
2023-04-25 20:38:19 +0600

    Создание,протипирование и первичная реализация нашего приложения

---
+++ src/main/java/ru/skillfactorydemo/tgbot/dto/GetCursOnDateXml.java
@@ -0,0 +1,18 @@
+package ru.skillfactorydemo.tgbot.dto;
+
+import lombok.Data;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlSchemaType;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+@XmlRootElement(name = "GetCursOnDateXML", namespace = "http://web.cbr.ru/")
+@Data
+public class GetCursOnDateXml {
+
+    @XmlElement(name = "On_date", required = true, namespace = "http://web.cbr.ru/") //Указание на то, в каком теге XML должно быть данное поле
+    protected XMLGregorianCalendar onDate;
+
+    public void setOnDate(XMLGregorianCalendar xmlGregCal) {
+    }
+}

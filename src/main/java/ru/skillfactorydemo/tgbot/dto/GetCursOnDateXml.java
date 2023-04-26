d7b54ebfca644d2f7e38fe5973e4a87abc5717bc
Symbat-Ikambayeva <sumbat2307@mail.ru>
2023-04-26 22:31:21 +0600

    3/7  2. Доработка нашего приложения и использование Telegram API, а также рефакторинг

--- src/main/java/ru/skillfactorydemo/tgbot/dto/GetCursOnDateXml.java
+++ src/main/java/ru/skillfactorydemo/tgbot/dto/GetCursOnDateXml.java
@@ -13,6 +13,4 @@ public class GetCursOnDateXml {
     @XmlElement(name = "On_date", required = true, namespace = "http://web.cbr.ru/") //Указание на то, в каком теге XML должно быть данное поле
     protected XMLGregorianCalendar onDate;
 
-    public void setOnDate(XMLGregorianCalendar xmlGregCal) {
-    }
 }
